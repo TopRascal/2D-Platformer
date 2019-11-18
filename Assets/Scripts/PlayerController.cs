aca8d018b5387c7bc77e1a719ba46265ccc924af
Pruitt_Nolan <pruin147@hsd.k12.or.us>
2019-11-18 09:03:50 -0800

    Added Plyer controller script.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody m_rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        m_rb.AddForce(new Vector3(movement, 0.0F, 0.0F));
+    }
+}
